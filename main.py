d600225785e59ce9f6e506fa943d49afdaf4465c
Jaswanthraj R <jaswanthraj.r@gmail.com>
2023-06-12 13:32:46 +0530

    Started

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Wings1 for T4 Flask")
+from flask import Flask
+from flask_restx import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+"""
+Lazily with the factory pattern
+
+from flask import Flask
+from flask_restx import Api
+
+api = Api()
+app = Flask(__name__)
+api.init_app(app)
+"""
+
+@api.route('/hello', '/world')
+class HelloWord(Resource):
+    def get(self):
+        return {'hello': 'world TCS'}
+    
+if __name__ == '__main__':
+    app.run(debug=True)
